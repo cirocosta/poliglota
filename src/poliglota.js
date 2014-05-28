5e0b4fc127f1c783e12dfc2351d9651967968134
Ciro S. Costa <ciro.costa@usp.br>
2014-05-27 21:55:33 -0300

    fixes #4

--- src/poliglota.js
+++ src/poliglota.js
@@ -6,40 +6,41 @@ var qs = require('querystring');
 var Q = require('q');
 
 function TranslateApi () {
-	this.API_URL = 'http://translate.google.com.br/translate_a/t?client=t&hl=en&sl=auto&ie=UTF-8&oe=UTF-8&multires=1&otf=2&ssel=0&tsel=0&sc=1&';
+  this.API_URL = 'http://translate.google.com.br/translate_a/t?client=t&hl=en&sl=auto&ie=UTF-8&oe=UTF-8&multires=1&otf=2&ssel=0&tsel=0&sc=1&';
 }
 
 TranslateApi.prototype._normalizeResult = function(result) {
-	return JSON.parse(result.replace(/,{2,}/g,',')
-	                    		  .replace(',]',']'))[0][0][0];
+  return JSON.parse(result.replace(/,{2,}/g,',')
+                            .replace(',]',']')
+                            .replace('[,', '['))[0][0][0];
 };
 
 TranslateApi.prototype.translate = function (text, toLang, srcLang) {
-	var def = Q.defer();
-	var data;
-	var url;
-	var scope = this;
+  var def = Q.defer();
+  var data;
+  var url;
+  var scope = this;
 
-	if (!(text && toLang))
-		def.reject(new Error('No text and lang passed.'));
+  if (!(text && toLang))
+    def.reject(new Error('No text and lang passed.'));
 
-	data = {
-		text: text,
-		tl: toLang,
-		sl: srcLang || 'auto'
-	};
+  data = {
+    text: text,
+    tl: toLang,
+    sl: srcLang || 'auto'
+  };
 
-	request(this.API_URL + qs.stringify(data), {
-		'User-Agent': 'Mozilla/5.0'
-	}, function (err, r, body) {
-		if (!err && r.statusCode === 200) {
-			def.resolve(scope._normalizeResult(r.body));
-		} else {
-			def.reject(err);
-		}
-	});
+  request(this.API_URL + qs.stringify(data), {
+    'User-Agent': 'Mozilla/5.0'
+  }, function (err, r, body) {
+    if (!err && r.statusCode === 200) {
+      def.resolve(scope._normalizeResult(r.body));
+    } else {
+      def.reject(err);
+    }
+  });
 
-	return def.promise;
+  return def.promise;
 };
 
 module.exports = new TranslateApi();
--- test/test-api.js
+++ test/test-api.js
@@ -8,10 +8,17 @@ describe('poliglota,', function () {
 
 	describe('_normalizeResult,', function () {
 
-		var result = '[[["The book is on the table","O livro está sobre a mesa","",""]],,"pt",,[["The book",[1],true,false,731,0,2,0],["is on the table",[2],true,false,722,2,6,0]],[["O livro",1,[["The book",731,true,false],["This book",0,true,false]],[[0,7]],"O livro está sobre a mesa"],["está sobre a mesa",2,[["is on the table",722,true,false],["on the table",0,true,false],["have on the table",0,true,false]],[[8,25]],""]],,,[["pt"]],1]';
-
 		it('should handle well what comes from the api', function () {
+			var result = '[[["The book is on the table","O livro está sobre a mesa","",""]],,"pt",,[["The book",[1],true,false,731,0,2,0],["is on the table",[2],true,false,722,2,6,0]],[["O livro",1,[["The book",731,true,false],["This book",0,true,false]],[[0,7]],"O livro está sobre a mesa"],["está sobre a mesa",2,[["is on the table",722,true,false],["on the table",0,true,false],["have on the table",0,true,false]],[[8,25]],""]],,,[["pt"]],1]';
 			var expected = 'The book is on the table';
+			var actual = poliglota._normalizeResult(result);
+
+			assert.equal(actual, expected);
+		});
+
+		it('should not raise error with \'não\'', function () {
+			var result = '[[["not","não","",""]],[["particle",["not","no","non","nay","nope"],[["not",["não"],,0.51075],["no",["não"],,0.059123605],["non",["não"],,0.035860281],["nay",["não"],,1.9526267e-05],["nope",["não"],,3.3405504e-06]],"não",19]],"pt",,[["not",[1],true,false,938,0,1,0]],[["não",1,[["not",938,true,false],["no",61,true,false],["non",0,true,false],["do not",0,true,false],["does not",0,true,false]],[[0,3]],"não"]],,[,"não-",[10],,true],[],2]';
+			var expected = 'not';
 			var actual = poliglota._normalizeResult(result);
 
 			assert.equal(actual, expected);
